96a9b49b9815954a5c3391297ee605759d663de0
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-27 14:13:47 +0900

    入力された値の処理実装

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ new Vue({
     name: "A山B郎",
     course: "Webエンジニアコース",
     acceptancePeriod: "2019年01月期",
-    defaultLastId: 3,
+    defaultLastId: 4,
     students: [
       {
         id: 1,
@@ -31,8 +31,13 @@ new Vue({
   // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
   methods: {
     addStudent: function () {
-      // この function() { } の中にaddStudentメソッド
-      // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+      this.students.push({
+        id: this.defaultLastId++,
+        name: this.name,
+        course: this.course,
+        acceptancePeriod: this.acceptancePeriod,
+      });
+      (this.name = ""), (this.course = ""), (this.acceptancePeriod = "");
     },
   },
 });
